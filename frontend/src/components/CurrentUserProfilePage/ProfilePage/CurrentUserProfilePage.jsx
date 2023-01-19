0965e910342831e2a1534c946471fbd6d1887f66
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-01-19 00:55:47 -0800

    profile options page

--- frontend/src/components/CurrentUserProfilePage/ProfilePage/CurrentUserProfilePage.jsx
+++ frontend/src/components/CurrentUserProfilePage/ProfilePage/CurrentUserProfilePage.jsx
@@ -15,7 +15,8 @@ const CurrentUserProfilePage = () => {
   const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user);
   const userId = sessionUser._id
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
   // const { userId } = useParams()
   // const [tabVal, setTabVal] = useState("current")
   // const [skellies, setSkellies] = useState({})
@@ -43,26 +44,42 @@ const CurrentUserProfilePage = () => {
       </div>
     )} else {
   return (
-    <div className='general-profile-container'>
-      <div className='general-profile-left'>
-        
+    <>
+      <div className='general-profile-container'>
+        <div className='general-profile-left'>
+          <div className='profile-top'>
+            <h1 className='current-user-greeting'>Hello <span id="current-user-username">{`${sessionUser.username}`}</span>,</h1>
+            <h2 className='current-user-sub-greeting'>what would you like to do today?</h2> 
+            <hr className='current-user-hr'/>
+          </div>
+          <div className="current-user-options-container">
+            <ul className='current-user-options'>
+              <li className='user-option'>Go to my Page</li>
+              <li className='user-option'>Change Profile Settings</li>
+              <li className='user-option'>Don't think of clicking me. Don't do it.</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='general-profile-right'>
+          <img className='general-profile-right'
+            id="skull-buddy-ani"
+            src={AniFrame1}
+          />
+        </div>
       </div>
-      <div className='profile-top'>
-        <h1 className='current-user-greeting'>Hello <span id="current-user-username">{`${sessionUser.username}`}</span>,</h1>
-        <h2 className='current-user-sub-greeting'>what would you like to do today?</h2> 
-        <hr className='current-user-hr'/>
+      <div id="current-user-whitespace">
+        <br className='whitespace' />
+        <br className='whitespace' />
+        <br className='whitespace' />
+        <br className='whitespace' />
+        <br className='whitespace' />
+        <br className='whitespace' />
+        <br className='whitespace' />
+        {/* <br className='whitespace' /> */}
+
       </div>
-      <div className="current-user-options-container">
-        <ul className='current-user-options'>
-          <li className='user-option'>Go to my Page</li>
-          <li className='user-option'>Change Profile Settings</li>
-          <li className='user-option'>Don't think of clicking me. Don't do it.</li>
-        </ul>
-      </div>
-       <div className='general-profile-right'>
-        
-      </div>
-    </div>
+    </>
   )}
 }
 
