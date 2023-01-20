7d4e84751096fd7b6e34a33d7b9ae588a84abfdd
remnantsofred <remnantsofred@gmail.com>
2023-01-19 22:47:00 -0800

    Started dropdown; populates with usernames

--- backend/routes/api/bones.js
+++ backend/routes/api/bones.js
@@ -90,7 +90,7 @@ router.delete('/skeletons/:skeletonId/:id', requireUser, async (req, res, next) 
       return next(error);
     }
     await bone.remove();
-    await Skeleton.updateOne({_id: bone.skeleton}, {$pull: {bones: bone._id}});
+    await Skeleton.updateOne({_id: bone.skeleton}, {$pull: {bones: bone}});
     return res.json(bone);
   }
   catch(err) {
@@ -141,7 +141,7 @@ router.post('/skeletons/:skeletonId', requireUser, validateBoneInput, async (req
       });
       
       let bone = await newBone.save();
-      await Skeleton.updateOne({_id: bone.skeleton}, {$push: {bones: bone._id}});
+      await Skeleton.updateOne({_id: bone.skeleton}, {$push: {bones: bone}});
       // bone = await bone.populate('skeleton', '_id, text');
       return res.json(bone);
     }
