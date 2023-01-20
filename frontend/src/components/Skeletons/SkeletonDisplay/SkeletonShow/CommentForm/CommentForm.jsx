40f8227e613e97a992c49d34aed7f17ab1939cc5
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-01-20 14:47:53 -0800

    comment has bugs

--- frontend/src/components/Skeletons/SkeletonDisplay/SkeletonShow/CommentForm/CommentForm.jsx
+++ frontend/src/components/Skeletons/SkeletonDisplay/SkeletonShow/CommentForm/CommentForm.jsx
@@ -7,9 +7,6 @@ import { useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
 import { fetchUsers, getUsers } from "../../../../../store/users";
 import { useEffect } from 'react';
-import { format } from 'date-fns'
-
-
 
 const CommentForm = ({comment, skeleton}) => {
   const user = useSelector(state => state.session.user);
@@ -20,27 +17,26 @@ const CommentForm = ({comment, skeleton}) => {
   const dispatch = useDispatch();
   
   const handleDelete = (e) => {
-      e.preventDefault();
-      dispatch(deleteComment(comment._id, skeleton._id));
-      e.target.value = "";
+    e.preventDefault();
+    dispatch(deleteComment(comment._id, skeleton._id));
+    e.target.value = "";
   }
 
   const handleUpdateSubmit = (e) => {
-      e.preventDefault();
-      const commentToUpdate = {"author": user._id, "text": updatedComment, "parent": skeleton._id, "_id": comment._id}
-      dispatch(updateComment(commentToUpdate));
-      setUpdatingComment(false);
-      e.target.value = "";
-
+    e.preventDefault();
+    const commentToUpdate = {"author": user._id, "text": updatedComment, "parent": skeleton._id, "_id": comment._id}
+    dispatch(updateComment(commentToUpdate));
+    setUpdatingComment(false);
+    e.target.value = "";
   }
 
   const handleShowUpdateField = (e) => {
-      e.preventDefault();
-      if (updatingComment) {
-      setUpdatingComment(false);
-      } else {
-        setUpdatingComment(true)
-      }
+    e.preventDefault();
+    if (updatingComment) {
+    setUpdatingComment(false);
+    } else {
+    setUpdatingComment(true)
+    }
   }
 
 
@@ -48,6 +44,9 @@ const CommentForm = ({comment, skeleton}) => {
 
 
   if (comment._id){
+    const commenterId = (comment.author._id)
+    // console.log(commenterId)
+    console.log(user)
       return (
         <>
         
@@ -57,16 +56,15 @@ const CommentForm = ({comment, skeleton}) => {
                     <h3 className="commenter-username" id="commenter-username">{comment.author.username}</h3>
                 </Link>
                 <p id="comment-timestamp"> {Date(comment.createdAt)}</p>
-                <div className="comment-body" style={{display: !updatingComment ? "block" : "none"}} >~ " {comment.text} "</div>
+                <div className="comment-body" style={{display: !updatingComment ? "block" : "none"}}>~ " {comment.text} "</div>
                 <div className="" style={{display: updatingComment ? "block" : "none"}}>
-                    <textarea className="comment-update-input" id="comment-update-input" placeholder="Update Comment" onChange={(e) => setUpdatedComment(e.target.value)} value={updatedComment} rows="3" columns="40" name=""/>
-                    {/* <button className="comment-save-update-button" onClick={handleUpdateSubmit}>Save Comment</button> */}
+                    <textarea className="comment-update-input" id="comment-update-input" placeholder="Update Comment" onChange={(e) => setUpdatedComment(e.target.value)} value={updatedComment} rows="3" columns="40"/>
                 </div>
             <div className="owner-comment-class-actions-container">
-                { (user._id === comment.author._id ) ? <button className="comment-update-button" onClick={handleShowUpdateField}>Edit</button> : <></>}
-                <button className="comment-save-update-button" onClick={handleUpdateSubmit} style={{display: updatingComment ? "block" : "none"}}>Save Comment</button>
+                { (user._id === commenterId ) ? <button className="comment-update-button" onClick={handleShowUpdateField}>Edit</button> : <></>}
+                { (user._id === commenterId ) ? <button className="comment-save-update-button" onClick={handleUpdateSubmit} style={{display: updatingComment ? "block" : "none"}}>Save Comment</button> : <></> }
 
-                { (user._id === comment.author._id ) ? <button className="comment-delete-button" onClick={handleDelete} >Delete</button> : <></>}    
+                { (user._id === commenterId ) ? <button className="comment-delete-button" onClick={handleDelete} >Delete</button> : <></>}    
             </div>
     
             </div>
