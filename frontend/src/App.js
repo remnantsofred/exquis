8d16df741afbf442763288e158d59fa2ceec0d1e
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-01-22 00:37:09 -0800

    added lil loading dude

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ import SkeletonForm from './components/Skeletons/SkeletonForm/SkeletonForm.jsx';
 import SkeletonShow from './components/Skeletons/SkeletonDisplay/SkeletonShow/SkeletonShow';
 
 import UpdateForm from './components/SessionForms/UpdateForm';
-
+import Loading from './components/Loading/Loading';
 
 
 function App() {
@@ -45,6 +45,7 @@ function App() {
         <ProtectedRoute exact path="/edit/profile" component={UpdateForm} />
 
         <Route exact path="/users/:userId" component={ProfilePage} />
+        <Route exact path="/loading" component={Loading} />
       </Switch>
       <Footer />
     </>
