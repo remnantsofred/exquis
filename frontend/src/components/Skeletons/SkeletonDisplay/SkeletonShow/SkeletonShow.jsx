d46789322258edd82dd9e0d23774accab94052b9
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-01-22 17:53:34 -0800

    final add

--- frontend/src/components/Skeletons/SkeletonDisplay/SkeletonShow/SkeletonShow.jsx
+++ frontend/src/components/Skeletons/SkeletonDisplay/SkeletonShow/SkeletonShow.jsx
@@ -11,7 +11,6 @@ import UpvoteButton from "../../UpvoteButton"
 import CollaboratorColorMatch from "./CollaboratorColorMatch/CollaboratorColorMatch"
 import CollaboratorsListMap from "./CollaboratorsListMap"
 import CurrentCollaboratorFxn from "./CurrentCollaboratorFxn"
-import PlaceBones from "./PlaceBones"
 import NewPlaceBones from "./PlaceBones"
 import NewBoneInput from "./NewBoneInput/NewBoneInput"
 import CommentForm from "./CommentForm/CommentForm"
@@ -134,8 +133,6 @@ const SkeletonShow = () => {
                 <div className="skeleton-body-input-container">
                     <div id="skeleton-body">
                       <NewPlaceBones skellie={skellie} colorArr={colorArr} />
-                      <PlaceBones skellie={skellie} colorArr={colorArr} />
-
                     </div> 
                       <div className="user-input-div">
                         <hr id="body-input-divider" />
