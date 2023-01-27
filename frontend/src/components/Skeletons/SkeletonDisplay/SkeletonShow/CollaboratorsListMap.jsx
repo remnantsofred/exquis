ca4a3a26df62d3d97fb67b9ee39faf7ad2c83418
remnantsofred <remnantsofred@gmail.com>
2023-01-26 17:09:30 -0800

    Update for to htmlFor

--- frontend/src/components/Skeletons/SkeletonDisplay/SkeletonShow/CollaboratorsListMap.jsx
+++ frontend/src/components/Skeletons/SkeletonDisplay/SkeletonShow/CollaboratorsListMap.jsx
@@ -9,8 +9,8 @@ const CollaboratorsListMap = ({colorArr, skellie}) => {
     const colorObj = colorArr.find(color => color.author === collaborator._id)
     const color = colorObj.color
     listie.push(
-    <Link to={`/users/${collaborator._id}`} className="skeleton-show-profile-link">
-      <h2 key={collaborator._id} style={{color: `${color}`}}> {collaborator.username} </h2>
+    <Link to={`/users/${collaborator._id}`} className="skeleton-show-profile-link" key={collaborator._id}>
+      <h2 style={{color: `${color}`}}> {collaborator.username} </h2>
     </Link>
   )
   })
