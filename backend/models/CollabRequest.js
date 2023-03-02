fef958b9e033c73fe3ac68d4b849cecbc0d387b4
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2023-03-02 13:41:00 -0800

    starting collab request schema

---
+++ backend/models/CollabRequest.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CollabRequestSchema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    // required: true
+  },
+  text: {
+    type: String
+  },
+  skeleton: {
+    type: Schema.Types.ObjectId,
+    ref: 'Skeleton',
+    // required: true
+  },
+}, {
+  timestamps: true
+});
+
+const skeletonSchema = Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }, 
+  prompt: {
+    type: String,
+  },
+  maxBones: {
+    type: Number,
+    required: true
+  },
+  maxCollaborators: {
+    type: Number
+  },
+  collaborators: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }], 
+  bones: [BoneSchema],
+
+  tags: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Tag'
+  }],
+  likes: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: 'Like'
+  }],
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Comment'
+  }]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('CollabRequest', collabRequestSchema);
